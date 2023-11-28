48dec54c611bf78d7901fdb9d2b95073b8a311f2
Ofir Katz <ofirkatz94@gmail.com>
2023-11-28 22:25:00 +0100

    feat: adding stories for the different states

--- src/MoneyInput/MoneyInput.stories.tsx
+++ src/MoneyInput/MoneyInput.stories.tsx
@@ -1,8 +1,8 @@
-import { Meta } from '@storybook/react'
+import type { Meta } from '@storybook/react'
 import MoneyInput from './MoneyInput'
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
-export default {
+const meta  = {
   title: 'Components/MoneyInput',
   component: MoneyInput,
   parameters: {
@@ -12,8 +12,65 @@ export default {
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
   tags: ['autodocs'],
   // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
-  argTypes: {},
+  argTypes: {
+    disabled: {
+      //
+    },
+
+  },
 } satisfies Meta<typeof MoneyInput>
 
+export default meta;
+
+type Story = Meta<typeof MoneyInput>;
+
 // More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
-export const Default = {}
+export const Primary: Story = {
+  args: {
+    //
+  },
+};
+
+
+export const PrimaryHover: Story = {
+  parameters: {
+    pseudo: { hover: true }
+  },
+};
+
+
+export const PrimaryFocus: Story = {
+  parameters: {
+    pseudo: { focus: true}
+  },
+};
+
+export const PrimaryError: Story = {
+  args: { required: true }
+};
+
+export const PrimaryErrorAndHover: Story = {
+  args: { required: true },
+  parameters: {
+    pseudo: { hover: true }
+  },
+};
+
+export const PrimaryErrorAndFocus: Story = {
+  args: { required: true },
+  parameters: {
+    pseudo: {focus: true }
+  },
+};
+
+export const PrimaryDisabled: Story = {
+  args: { disabled: true }
+};
+
+
+export const PrimaryDisabledAndFocus: Story = {
+  args: { disabled: true },
+  parameters: {
+    pseudo: {focus: true}
+  },
+};
